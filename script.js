a8a90117d873dde8192a383024cff2a1962534c6
Elsa Zeigler <elsa.head@gmail.com>
2022-08-02 13:31:05 -0400

    Delete divs when a book is deleted from the library

--- script.js
+++ script.js
@@ -7,71 +7,83 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
-function createDiv(newBook) {
-    let index=myLibrary.length-1;
-    const bookDiv = document.createElement('div')
-    document.getElementById('book_list').appendChild(bookDiv)
-    bookDiv.className = "book_box";
-    bookDiv.id = "book"+index;
-    
-    const titleDiv = document.createElement('div')
-    titleDiv.classList.add('titleDiv');
-    titleDiv.textContent = newBook.title
-    titleDiv.className = "book_title";
-    bookDiv.appendChild(titleDiv)
-    
-    const authorDiv = document.createElement('div')
-    authorDiv.classList.add('authorDiv');
-    authorDiv.textContent = newBook.author
-    authorDiv.className = "author_name";
-    bookDiv.appendChild(authorDiv)
-
-    const pagesDiv = document.createElement('div')
-    pagesDiv.classList.add('titleDiv');
-    pagesDiv.textContent = newBook.pages
-    pagesDiv.className = "book_title";
-    bookDiv.appendChild(pagesDiv)
-
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    checkbox.classList.add('checkbox');
-    if(newBook.read.checked == true){
-        checkbox.checked = true
-    }else{
-        checkbox.checked = false
-    }
-
-    checkbox.className = "checkbox";
-    bookDiv.appendChild(checkbox);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('deleteButton');
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "delete";
-    deleteButton.id = index;
-    bookDiv.appendChild(deleteButton);
-
-    document.getElementById(index).addEventListener('click', deleteBook);
-
-    function deleteBook() {
-        console.log("delete")
-    }
-}
-
-
 function addBookToLibrary() {
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
-    let read = document.getElementById('read');
+    let read = document.getElementById('read').checked;
     const newBook = new Book(title,author,pages,read);
     myLibrary.push(newBook)
-    console.log(myLibrary)
-    console.log(newBook.read.checked)
-    createDiv(newBook)
+    createDiv(myLibrary)
+}
+
+const parent = document.getElementById('book_list')
+
+function createDiv(library) {
+
+    clearCards(parent);
+
+    for(let i=0; i<library.length; i++){
+        const bookDiv = document.createElement('div')
+        document.getElementById('book_list').appendChild(bookDiv)
+        bookDiv.className = "book_box";
+
+        const titleDiv = document.createElement('div')
+        titleDiv.classList.add('titleDiv');
+        titleDiv.textContent = library[i].title
+        titleDiv.className = "book_title";
+        bookDiv.appendChild(titleDiv)
+
+        const authorDiv = document.createElement('div')
+        authorDiv.classList.add('authorDiv');
+        authorDiv.textContent = library[i].author
+        authorDiv.className = "author_name";
+        bookDiv.appendChild(authorDiv)
+    
+        const pagesDiv = document.createElement('div')
+        pagesDiv.classList.add('titleDiv');
+        pagesDiv.textContent = library[i].pages
+        pagesDiv.className = "book_title";
+        bookDiv.appendChild(pagesDiv)
+    
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.classList.add('checkbox');
+        if(library[i].read == true){
+            checkbox.checked = true
+        }else{
+            checkbox.checked = false
+        }
+        checkbox.className = "checkbox";
+        bookDiv.appendChild(checkbox);
+    
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('deleteButton');
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "delete";
+        deleteButton.id=i
+        bookDiv.appendChild(deleteButton);
+
+
+        deleteButton.addEventListener('click', removeBook => {
+            let button = document.getElementById(i);
+            for(let j = 0; j<library.length; j++){
+                if(button.id == j){
+                    library.splice(j, 1)
+                }
+                createDiv(library)
+            }
+        })
+
+    }
+ 
 }
 
 
+function clearCards(parent) {
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild)
+    }
+}
 
 document.getElementById('add').addEventListener('click', addBookToLibrary);
-
